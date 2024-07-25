a3828187b801117a2edf569406637d7c31ee0bfc
Federica Rocchetti <rocchetti.federica@gmail.com>
2024-07-25 09:45:21 +0200

    creeato product list component

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [MatIconModule,],
+  imports: [MatIconModule,RouterOutlet],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.css'
 })
