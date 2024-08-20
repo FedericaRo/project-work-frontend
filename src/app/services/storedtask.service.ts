7780d1453ba1199ba856c2b9317941e28de735b8
Francesca <francesca@email.com>
2024-08-20 19:06:49 +0200

    modifiche per le stored task

--- src/app/services/storedtask.service.ts
+++ src/app/services/storedtask.service.ts
@@ -1,28 +1,30 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { StoredTask } from '../model/StoredTask';
 
 @Injectable({
   providedIn: 'root'
 })
-export class StoredtaskService 
-{
+export class StoredtaskService {
+  private apiUrl = 'api/storedTasks';
+  private newStoredTaskUrl = 'api/storedTasks/newStoredTask';
 
-  constructor(private http:HttpClient) { }
-  
-  getAll(): Observable<StoredTask[]>
-  {
-    return this.http.get<StoredTask[]>("/api/storedtask");
+  constructor(private http: HttpClient) {}
+
+  getAll(): Observable<StoredTask[]> {
+    return this.http.get<StoredTask[]>(this.apiUrl);
   }
 
-  delete(id:number): Observable<void>
-  {
-    return this.http.delete<void>(`/api/storedtask/${id}`)
+  addNewStoredTask(storedTask: StoredTask): Observable<StoredTask> {
+    return this.http.post<StoredTask>(this.newStoredTaskUrl, storedTask);
   }
 
-  addNewStoredTask(storedtask:StoredTask):Observable<StoredTask>
-  {
-    return this.http.post<StoredTask>(`/api/storedtask/newstoredtask`, storedtask)
+  update(id: number, storedTask: StoredTask): Observable<StoredTask> {
+    return this.http.put<StoredTask>(`${this.apiUrl}/${id}`, storedTask);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
   }
 }
